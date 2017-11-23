0f5961481bd36c9968afe9c35b73685863f63710
Sajidah Akther <sajidah13@hotmail.co.uk>
2017-11-23 17:01:17 +0000

    first lab

---
+++ Week 2/ComputeArea/src/computearea/ComputeArea.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package computearea;
+
+/**
+ *
+ * @author sajidahakther
+ */
+public class ComputeArea {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int radius = 20;
+    double area = radius * radius * 3.14159;
+System.out.println("The area for the circle of radius " + radius + " is " +
+area);
+    }
+    
+}
---
+++ Week 2/Employee/src/employee/Employee.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package employee;
+
+/**
+ *
+ * @author sajidahakther
+ */
+public class Employee {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int Id = 1000;
+        String name = "Jack Hunt";
+        int age = 40;
+        double salary = 99999.99;
+        
+        System.out.println(Id +" " + name + " " + age  + " "+ salary);
+    }
+    
+}
