07038e820b6c3443c154eb8a3d33472beb4cce80
Sajidah Akther <sajidah13@hotmail.co.uk>
2017-11-23 17:26:51 +0000

    added week2

--- week2/numbers/Numbers.java
+++ week2/numbers/Numbers.java
@@ -16,10 +16,11 @@ public class Numbers {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        for (int i =0; i <=2000; i++) {
-        if((i%2)==0)
-    System.out.println(i);
+        for (int i = 0; i <= 2000; i++) {
+            if ((i % 2) == 0) {
+                System.out.println(i);
+            }
         }
-   
-    
+
+    }
 }
